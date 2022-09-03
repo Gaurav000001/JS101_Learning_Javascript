3f3efe0e089fed942ab31db5ff3607aac996ddfc
Gaurav <gangulygaurav166@gmail.com>
2022-09-03 22:27:01 +0530

    Mathematical Operators

---
+++ Mathematical-Operators/problem3.js
@@ -0,0 +1,15 @@
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+
+var one = 1;
+var two = 4;
+var three = 9;
+var four = 12;
+var five = 10;
+var six = 12;
+
+console.log(one+two+three+four+five+six);
